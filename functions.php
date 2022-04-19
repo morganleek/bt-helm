79685191d5bc20401802ef22f96325f18e9203ac
Morgan Leek <morganleek@gmail.com>
2022-04-19 16:03:34 +0800

    first layout

--- functions.php
+++ functions.php
@@ -33,11 +33,13 @@
 				'regular'
 			);
 			// Enqueue a few of our entry points
+			add_action( 'init', [$this, 'bones_theme_font_styles' ], 10 );
 			add_action( 'wp_enqueue_scripts', [ $this, 'plugin_enqueue' ] );
 			add_action( 'after_setup_theme', [ $this, 'bones_theme_support' ] );
-			add_action( 'admin_init', [$this, 'bones_theme_editor_styles' ] );
+			add_action( 'admin_init', [$this, 'bones_theme_font_styles' ] );
 			add_action( 'wp_head', [ $this, 'bones_theme_preload_webfonts' ] );
 			add_action( 'init', [ $this, 'bones_name_register_block_styles' ], 100 );
+			add_filter( 'body_class', [$this, 'bones_theme_body_class'], 20, 2 );
 		}
 
 		public function plugin_enqueue() {
@@ -82,22 +84,48 @@
 			}
 		}
 
-		public function bones_theme_editor_styles() {
+		public function bones_theme_font_styles() {
 			wp_add_inline_style( 'wp-block-library', $this->bones_theme_get_font_face_styles() );
 		}
 
 		public function bones_theme_get_font_face_styles() {
-			return "";
-			// return "
-			// @font-face{
-			// 	font-family: 'Source Serif Pro';
-			// 	font-weight: 200 900;
-			// 	font-style: normal;
-			// 	font-stretch: normal;
-			// 	font-display: swap;
-			// 	src: url('" . get_theme_file_uri( 'assets/fonts/SourceSerif4Variable-Roman.ttf.woff2' ) . "') format('woff2');
-			// }
-			// ";
+			return "
+				@font-face {
+					font-family: 'national-compressed-extrabold';
+					src: url('" . get_theme_file_uri( 'assets/fonts/national2compressed-extrabold.woff2' ) . "') format('woff2'),
+							url('" . get_theme_file_uri( 'assets/fonts/national2compressed-extrabold.woff' ) . "') format('woff');
+					font-weight: normal;
+					font-style: normal;
+
+				}
+				
+				@font-face {
+					font-family: 'geoslab703_md_btlight';
+					src: url('" . get_theme_file_uri( 'assets/fonts/geometricslab703bt-light-webfont.woff2' ) . "') format('woff2'),
+							url('" . get_theme_file_uri( 'assets/fonts/geometricslab703bt-light-webfont.woff' ) . "') format('woff');
+					font-weight: normal;
+					font-style: normal;
+
+				}
+
+				@font-face {
+					font-family: 'geoslab703_md_btmedium';
+					src: url('" . get_theme_file_uri( 'assets/fonts/geometricslab703bt-medium-webfont.woff2' ) . "') format('woff2'),
+							url('" . get_theme_file_uri( 'assets/fonts/geometricslab703bt-medium-webfont.woff' ) . "') format('woff');
+					font-weight: normal;
+					font-style: normal;
+
+				}
+
+				@font-face {
+					font-family: 'geoslab703_md_btbold';
+					src: url('" . get_theme_file_uri( 'assets/fonts/geometricslab703bt-bold-webfont.woff2' ) . "') format('woff2'),
+							url('" . get_theme_file_uri( 'assets/fonts/geometricslab703bt-bold-webfont.woff' ) . "') format('woff');
+					font-weight: normal;
+					font-style: normal;
+
+				}
+			";
 		}
 
 		public function bones_theme_preload_webfonts() {
@@ -122,6 +150,12 @@
 				'name' => 'oversized',
 				'label' => __( 'Oversized', 'bones_name' ),
 			] );
+
+			// Header
+			register_block_style( 'core/heading', [
+				'name' => 'narrow',
+				'label' => __( 'Narrow', 'bones_name' ),
+			] );
 	
 			// Columns
 			// register_block_style( 'core/columns', [
@@ -144,6 +178,15 @@
 			//   'name' => 'play',
 			//   'label' => __( 'Play', 'bones_name' ),
 			// ] );
+		}
+
+		public function bones_theme_body_class( $classes, $class ) {
+			global $post;
+			if( $post->post_type == 'page' ) {
+				$classes[] = $post->post_type . '-' . $post->post_name;
+			}
+
+			return $classes;
 		}
 	}
 
